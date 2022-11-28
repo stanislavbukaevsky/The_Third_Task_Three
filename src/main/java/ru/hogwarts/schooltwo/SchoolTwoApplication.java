6a12a7229a40e7c1520ebd57a667f2b40a297242
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-11-28 15:27:02 +0300

    Первый коммит

---
+++ src/main/java/ru/hogwarts/schooltwo/SchoolTwoApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schooltwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTwoApplication.class, args);
+	}
+
+}
